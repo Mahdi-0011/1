c5c547d566939ea9610a226000b611c834c319c7
Mahdi <mahdi.mousavi0011@gmail.com>
2025-05-29 06:49:34 +0200

    first project react native

---
+++ app/Home.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import { Text, View } from "react-native";
+
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text>*Hello world*</Text>
+      <Text>my first project</Text>
+    </View>
+  );
+}
